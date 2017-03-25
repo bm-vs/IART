25c8f2221140172257454e866fd9baee43f590b6
Bruno Santos <bruno.santos1996@gmail.com>
2017-03-25 21:42:00 +0000

    decent generator

--- Project1/src/Truck.java
+++ Project1/src/Truck.java
@@ -1,14 +1,17 @@
 public class Truck {
 	private int fuel;
 	private int load;
+	private Location location;
 	
-	public Truck(int fuel, int load) {
+	public Truck(int fuel, int load, Location location) {
 		this.fuel = fuel;
 		this.load = load;
+		this.location = location;
 	}
 	
 	public void print() {
 		System.out.println(fuel + " " + load);
+		location.print();
 	}
 	
 }
